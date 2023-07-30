86111671662386b617275114435da94a7c28cfc3
Satrio Hutomo <strhtmo17@gmail.com>
2023-07-30 23:29:11 +0700

    fix query in address controller

--- controllers/address.controller.js
+++ controllers/address.controller.js
@@ -24,7 +24,7 @@ class Controller {
   static async get_address_user(req, res, next) {
     try {
       const { id } = req.user;
-      const user_address = await Address.findOne({ where: { user_id: id } });
+      const user_address = await Address.findAll({ where: { user_id: id } });
       res.status(200).json({ data: user_address });
     } catch (err) {
       next(err);
@@ -35,7 +35,7 @@ class Controller {
   static async edit_address_user(req, res, next) {
     try {
       const user_id = req.user.id;
-      const { id } = req.body;
+      const { id, title, province, city, district, zipcode } = req.body;
 
       const address_user = await Address.findOne({ where: { id } });
 
@@ -44,16 +44,16 @@ class Controller {
       }
 
       const edit_address = await Address.update(
-        { where: { user_id: user_id } },
         {
           title,
           province,
           city,
           district,
           zipcode,
-        }
+        },
+        { where: { user_id: user_id } }
       );
-      res.status(201).json({ message: `address user with ${edit_address.id} edited` });
+      res.status(201).json({ message: `address user with ${id} edited` });
     } catch (err) {
       next(err);
     }
@@ -64,7 +64,7 @@ class Controller {
     try {
       const { id } = req.body;
       const delete_address = await Address.destroy({ where: { id } });
-      res.status(200).json({ message: `address with ${delete_address.id} deleted` });
+      res.status(200).json({ message: `address with ${id} deleted` });
     } catch (err) {
       next(err);
     }
