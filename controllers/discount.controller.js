57bb0dc7fc0476d56ab95a513cf712257d70ee3f
Ilman A <128614881+Ilmanzi@users.noreply.github.com>
2023-08-01 15:11:23 +0700

    initial push (#6)

---
+++ controllers/discount.controller.js
@@ -0,0 +1,10 @@
+const { Discount } = require("../models")
+
+class DiscountController {
+    static async create_discount(req,res,next) {
+        try {
+        } catch (err) {
+            next(err)
+        }
+    }
+}
